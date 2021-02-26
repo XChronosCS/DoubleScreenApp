1e1edf511706913c383adf4fc751b2bd5d00902d
XChronosCS <eezor1@binghamton.edu>
2021-02-25 20:40:37 -0500

    Worked some more on the project

--- app/src/main/java/com/example/doublescreen/SecondActivity.java
+++ app/src/main/java/com/example/doublescreen/SecondActivity.java
@@ -7,14 +7,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class SecondActivity extends AppCompatActivity {
 
     private Button mBtGoBack;
+
+    public enum Thoughts {
+        A("Down with the Patriarchy!"), B("Woop!"), C("Water is Wet!"), D("Gen 4 Remakes when?"),
+        E("Hope you're having a great day!"), F("I'm scared of heights!"), G("Banana!"),
+        H("The revolution is soon!"), I("I'm actually a ditto!"), J("I like trains."),
+        K("I should run for President!"), L("I wish I had hands."), M("Why can I use Ice Punch?"),
+        N("The meaning of life is 42"), O("Would I know if I was dumb?"), P("What happens when you close this app?"),
+        Q("Subs not Dubs."), R("Bubbles!"), S("My favorite color is blurple!");
+
+        public final String label;
+
+        private Thoughts(String label){
+            this.label = label;
+        }
+
+        public static Thoughts getRandomThought() {
+            Random random = new Random();
+            return values()[random.nextInt(values().length)];
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
 
         mBtGoBack = (Button) findViewById(R.id.button_Go_back);
 
@@ -26,6 +49,8 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     private void launchActivity() {
 
